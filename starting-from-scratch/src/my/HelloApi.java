9010c79caa451ec14bb17604cea359e674f04137
boatrain <yinger521999@gmail.com>
2020-09-24 09:24:44 +0800

    git initial

---
+++ starting-from-scratch/src/my/HelloApi.java
@@ -0,0 +1,5 @@
+package my;
+
+public interface HelloApi {
+    void sayHello();
+}  
---
+++ starting-from-scratch/src/my/HelloImpl.java
@@ -0,0 +1,8 @@
+package my;
+
+public class HelloImpl implements HelloApi {
+    @Override
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
+}  
